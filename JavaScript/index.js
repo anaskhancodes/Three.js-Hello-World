cd27c728064a632157fc2c274b18f06ba6e37a55
M.Anas Khan <a4anas406@gmail.com>
2023-10-06 00:44:52 +0500

    New Shapes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,37 +6,29 @@ const renderer = new THREE.WebGLRenderer(); // Corrected capitalization
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 3, 1); // Corrected typo
+// const geometry = new THREE.BoxGeometry(1, 3, 1); // BOX
+const geometry = new THREE.CapsuleGeometry(1, 0.5, 10, 10); // CAPSULE
+// const geometry = new THREE.CircleGeometry(1,50); // CIRCLE
+// const geometry = new THREE.ConeGeometry(1,2, 300); // CONE
+// const geometry = new THREE.CylinderGeometry(2 , 2, 2,50); // CYLINDER
 const material = new THREE.MeshBasicMaterial({ color: "yellow" });
-const cube = new THREE.Mesh(geometry, material);
-// const cube = new THREE.Line(geometry, material);
+// const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Line(geometry, material);
 
 scene.add(cube);
 renderer.render(scene, camera);
 
-let flag = true;
 
-function animate(){
-    
-    if(cube.position.z > 1){
-        flag = false
-    } else if(cube.position.z < -7){
-        flag = true
-    }
-    
-    if(flag){
-        cube.position.z += 0.1;
-    }else{
-        cube.position.z -= 0.1;
-    }
-    
-    cube.rotation.y += 0.1; 
-    cube.rotation.x += 0.01; 
-    cube.rotation.z += 0.01; 
+let q = 0;
 
+const animate = () => {
+    cube.position.x = 5 * Math.sin(q += 0.01);
+    
+    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.z += 0.01;
+    
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
 animate();
-
-// setInterval(animate, 100)
